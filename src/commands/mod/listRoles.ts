3e77ac363acd0b8ddf20a99815b87cfe80c9f215
nathan <nathan@kaizotrap.com>
2017-02-19 00:05:00 -0500

    Exclusion
    Updated command to exclude roles below "Rasputin", @everyone, and bots.

--- src/commands/mod/listRoles.ts
+++ src/commands/mod/listRoles.ts
@@ -24,29 +24,36 @@ export default class ListRoles extends Command<Bot>
         // variable declaration
         const guildStorage: any = this.bot.guildStorages.get(message.guild);
         const availableRoles: any = guildStorage.getItem('Server Roles');
-        const serverRoles: any = message.guild.roles;
+        const rasputinRole: Role = message.guild.roles.find('name', 'Rasputin');
+        const serverRoles: any = message.guild.roles;        
         let leftCol: string = '';
         let rightCol: string = '';
-
+        
         if (message.member.roles.find('name', 'Rasputin'))
         {
             // iterate through server roles to build leftCol/rightCol
             serverRoles.forEach(
                 function(el: any)
                 {
-                    leftCol += '\n' + el.name;
-                    if (util.existsInArray(availableRoles, el.name))
-                        rightCol += '\n**Allowed**';
-                    else
-                        rightCol += '\nNot Allowed';
+                    // grab all roles below rasputin, exclude @everyone and bots
+                    if (el.position < rasputinRole.position && el.name !== '@everyone' && el.managed === false)
+                    {
+                        leftCol += '\n' + el.name;
+                        if (util.existsInArray(availableRoles, el.name))
+                            rightCol += '\n**Allowed**';
+                        else
+                            rightCol += '\nNot Allowed';
+                    }
                 }
             );
 
             // build the output embed
             const modEmbed: RichEmbed = new RichEmbed()
                 .setTitle(message.guild.name + ': List of Roles')
+                .setColor(0x274E13)
                 .addField('Roles', leftCol, true)
-                .addField('Status', rightCol, true);
+                .addField('Status', rightCol, true)
+                .setFooter('*Roles are case-sensitive.');
             
             // display the list
             return message.channel.sendEmbed(modEmbed, '', { disableEveryone: true });
@@ -54,14 +61,14 @@ export default class ListRoles extends Command<Bot>
         else
         {
             // iterate through server roles to build leftCol/rightCol
-            availableRoles.forEach(
-                function(el: any) { leftCol += '\n' + el.name; }
-            );
+            availableRoles.forEach(function(el: any) { leftCol += '\n' + el.name; });
             
             // build the output embed
             const userEmbed: RichEmbed = new RichEmbed()
                 .setTitle(message.guild.name + ': List of Roles')
-                .addField('Roles', leftCol, true);
+                .setColor(0x274E13)
+                .addField('Roles', leftCol, true)
+                .setFooter('*Roles are case-sensitive.');
             
             // display the list
             return message.channel.sendEmbed(userEmbed, '', { disableEveryone: true });
