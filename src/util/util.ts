3604f2e394af93ca641e69d287c40637b0d11662
nathan <nathan@kaizotrap.com>
2017-02-19 00:50:03 -0500

    Refactor
    Thanks for the tip, zaj.

--- src/util/util.ts
+++ src/util/util.ts
@@ -1,19 +1,8 @@
 'use strict'
-
 export default class Util
 {
     public static existsInArray(array: Array<any>, item: string): boolean
     {
-        if (array === null)
-            return false;
-
-        let x: number = 0;
-        while (x < array.length)
-        {
-            if (array[x].name === item)
-                return true;
-            x++;
-        }
-        return false;
+        return Boolean(array.find(a => a.name === item));
     }
 }
